612aaec89c3f9b67a3330443cae54ba30538653b
Jaden80 <jyyoo0329@gmail.com>
2026-05-29 15:09:28 +0900

    fix: remove env from next config

--- next.config.mjs
+++ next.config.mjs
@@ -10,12 +10,7 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-  env: {
-    NEIS_API_KEY: process.env.NEIS_API_KEY,
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    DATA_GO_KR_API_KEY: process.env.DATA_GO_KR_API_KEY,
-    KAKAO_MAP_API_KEY: process.env.KAKAO_MAP_API_KEY,
-  }
+
 };
 
 export default nextConfig;
